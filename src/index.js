af699b6c48865a6f4e4a2d0d1fa13f7616c03443
envincebal <vincentyan1@gmail.com>
2018-03-14 20:33:03 -0700

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import Leaderboard from './App';
-
-ReactDOM.render(<Leaderboard />, document.getElementById('root'));
